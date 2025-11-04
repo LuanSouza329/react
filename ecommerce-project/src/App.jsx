3b67ac674fd46dcc18a0f4c3f0ec5b54e4266e1f
Luan <luansouza329@gmail.com>
2025-11-04 18:20:15 -0300

    Fazendo as outras páginas

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,17 +1,22 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route index element={<HomePage />} />
+      <Routes>
+        <Route index element={<HomePage />} />
 
-      <Route path="/checkout" element={<CheckoutPage />} />
-    </Routes>
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/tracking" element={<TrackingPage />} />
+
+      </Routes>
     </>
   )
 }
