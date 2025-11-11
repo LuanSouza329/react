832698288b938f143c457ecf82b7ff286fa92785
Luan <luansouza329@gmail.com>
2025-11-11 18:21:38 -0300

    Subimos o carrinho de comprar para o APP para o componente seja renderizado somente uma vez e seja compartilhado entre os componentes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Routes, Route } from "react-router";
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from "./pages/CheckoutPage";
@@ -7,12 +9,21 @@ import './App.css'
 
 function App() {
 
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items")
+      .then((response) => {
+        setCart(response.data);
+      })
+  },[])
+
   return (
     <>
       <Routes>
-        <Route index element={<HomePage />} />
+        <Route index element={<HomePage cart={cart} />} />
 
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/tracking" element={<TrackingPage />} />
 
