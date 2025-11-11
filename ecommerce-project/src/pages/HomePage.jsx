c22a5e065cfe950236cb145c71cf69478226f5b4
Luan <luansouza329@gmail.com>
2025-11-11 18:08:51 -0300

    Adicionando o carrinho

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -7,18 +7,25 @@ export function HomePage() {
 
     const [products, setProducts] = useState([]);
 
+    const [cart, setCart] = useState([]);
+
+
     useEffect(() => {
         axios.get("http://localhost:3000/api/products")
             .then((response) => {
                 setProducts(response.data);
+            });
+
+        axios.get("http://localhost:3000/api/cart-items")
+            .then((response) => {
+                setCart(response.data);
             })
     }, []);
 
 
-
     return (
         <>
-            <Header />
+            <Header  cart={cart}/>
             <title>E-commerce Project</title>
 
             <div className="home-page">
