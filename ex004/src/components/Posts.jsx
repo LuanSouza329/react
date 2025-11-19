baff71410893c24830959f76b9b688bc73947680
Luan <luansouza329@gmail.com>
2025-11-19 18:49:23 -0300

    Adição do asyn await nos fetchs

--- ex004/src/components/Posts.jsx
+++ ex004/src/components/Posts.jsx
@@ -1,27 +1,51 @@
 import { useState, useEffect } from "react";
+import { PostCard } from "./miniComponents/PostCard";
+import "./Post.css"
 
 export function Posts() {
 
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-            .then((response) => {
-                return response.json();
-            })
-            .then((response) => {
-                setPosts(response)
-            })
+        async function carregarDados() {
+            try {
+                setLoading(true);
+
+                const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+                if (!resp.ok) {
+                    throw new Error("Erro ao carregar os dados.");
+                }
+
+                const data = await resp.json();
+
+                setPosts(data);
+
+            } catch (error) {
+                console.error("Erro na requisição:", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        carregarDados();
+
     }, []);
 
+
     return (
-        <>
-            {posts.map((post)=>
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.body}</p>
-                </div>
+        <div>
+            {loading ? (
+                <div className="spinner">Carregando...</div>
+            ) : (
+                posts.map((post) => (
+                    <PostCard key={post.id} title={post.title} body={post.body} />
+                ))
             )}
-        </>
+        </div>
     );
+
+
 }
---
+++ ex004/src/components/miniComponents/PostCard.jsx
@@ -0,0 +1,8 @@
+export function PostCard({ title, body }) {
+    return (
+        <>
+            <h2>{title}</h2>
+            <p>{body}</p>
+        </>
+    );
+}
